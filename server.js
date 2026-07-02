f9aa651b0b653f32fb4f41b5e8ee7b82edd9e204
Gokul-Gokzzz <gokulunniu88@gmail.com>
2026-07-02 15:38:40 +0530

    port corrected

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(errorHandler);
 
 const { startJobs } = require('./src/jobs');
 
-server.listen(env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
   startJobs();
 });
