1b6f020d6ec681875e505a6694773f2bfde59913
Gokul-Gokzzz <gokulunniu88@gmail.com>
2026-07-02 15:07:16 +0530

    changes made

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
+  console.log(process.env.DATABASE_URL,"database URL:");
 }
 
 module.exports = prisma;
--- src/config/env.js
+++ src/config/env.js
@@ -1,21 +1,21 @@
-const Joi = require("joi");
+// const Joi = require("joi");
 
-const schema = Joi.object({
-  DATABASE_URL: Joi.string().required(),
-  REDIS_URL: Joi.string().required(),
-  JWT_SECRET: Joi.string().required(),
-  JWT_REFRESH_SECRET: Joi.string().required(),
-  PORT: Joi.number().default(5000),
-  FCM_SERVER_KEY: Joi.string().allow("").optional(),
-  RAZORPAY_KEY_ID: Joi.string().allow("").optional(),
-  RAZORPAY_KEY_SECRET: Joi.string().allow("").optional(),
-  RAZORPAY_WEBHOOK_SECRET: Joi.string().allow("").optional(),
-}).unknown();
+// const schema = Joi.object({
+//   // DATABASE_URL: Joi.string().required(),
+//   REDIS_URL: Joi.string().required(),
+//   JWT_SECRET: Joi.string().required(),
+//   JWT_REFRESH_SECRET: Joi.string().required(),
+//   PORT: Joi.number().default(5000),
+//   FCM_SERVER_KEY: Joi.string().allow("").optional(),
+//   RAZORPAY_KEY_ID: Joi.string().allow("").optional(),
+//   RAZORPAY_KEY_SECRET: Joi.string().allow("").optional(),
+//   RAZORPAY_WEBHOOK_SECRET: Joi.string().allow("").optional(),
+// }).unknown();
 
-const { error, value } = schema.validate(process.env);
+// const { error, value } = schema.validate(process.env);
 
-if (error) {
-  throw new Error(`Environment validation error: ${error.message}`);
-}
+// if (error) {
+//   throw new Error(`Environment validation error: ${error.message}`);
+// }
 
-module.exports = value;
+// module.exports = value;
